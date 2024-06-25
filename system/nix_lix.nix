ae3efb9c33d142576131c6f76e7fd4e43bd0c78e
Whovian9369 <Whovian9369@gmail.com>
2024-06-25 03:38:36 -0400

    Style edit for `system/nix_lix.nix`

--- system/nix_lix.nix
+++ system/nix_lix.nix
@@ -1,9 +1,11 @@
 {
-  nix.settings.extra-substituters = [
+  nix.settings = {
+    extra-substituters = [
     "https://cache.lix.systems"
-  ];
+    ];
 
-  nix.settings.trusted-public-keys = [
-    "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-  ];
+    trusted-public-keys = [
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+    ];
+  };
 }
