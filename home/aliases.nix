dcd3e742f81e43d5862f71b28e430103b8064e28
Whovian9369 <Whovian9369@gmail.com>
2024-09-26 02:44:18 -0400

    Move around shell aliases a little.

--- home/aliases.nix
+++ home/aliases.nix
@@ -1,5 +1,12 @@
+{ nixpkgs, options, ... }:
 {
   home.shellAliases = {
+  # From Nix environment
+    "7z" = "7zz";
+      # "7zz" is from "nixpkgs#_7zz"
+    "termbin" = "nc termbin.com 9999";
+      # Alias that lets me upload text to https://termbin.com/
+      # Mainly so I can lazily upload build logs.
   # From Windows "%PATH%"
     "adb" = "adb.exe";
     "caja" = "explorer.exe";
@@ -8,13 +15,6 @@
     "tailscale" = "tailscale.exe";
     "yt-dlp" = "yt-dlp.exe";
     "7zexe" = "/mnt/c/Program\\ Files/7-Zip/7z.exe";
-
-  # From Nix environment
-    "7z" = "7zz";
-      # "7zz" is from "nixpkgs#_7zz"
-    "termbin" = "nc termbin.com 9999";
-      # Alias that lets me upload text to https://termbin.com/
-      # Mainly so I can lazily upload build logs.
-  # } // nixpkgs.lib.optionalAttrs (nixpkgs.lib.options ? wsl) {
   };
+  # } // nixpkgs.lib.optionalAttrs (options ? wsl) {
 }
