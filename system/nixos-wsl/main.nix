a62fdbcf4c03ea9bc44d61ebfec2959dc93bddae
Whovian9369 <Whovian9369@gmail.com>
2024-11-20 11:15:10 -0500

    Formatting changes on system `main.nix` files.
    Mainly just reminders not to change `system.stateVersion` on live systems.

--- system/nixos-wsl/main.nix
+++ system/nixos-wsl/main.nix
@@ -71,6 +71,8 @@
     Need to look into this again at some point, I suppose.
   */
 
+##############################################################################
+
   /*
     This option defines the first version of NixOS you have installed on this particular machine,
     and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
