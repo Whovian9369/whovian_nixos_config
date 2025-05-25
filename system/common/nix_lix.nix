e84ac2cd384a9dd67791f1470a64e849e43ebf6c
Whovian9369 <Whovian9369@gmail.com>
2025-05-24 21:10:01 -0400

    Add commented-out use of nixbuild.net as remote builder.
    It seems useful whenever I'm on extremely hardware strained systems, so I'm wanting to keep it around just in case.

--- system/common/nix_lix.nix
+++ system/common/nix_lix.nix
@@ -10,4 +10,37 @@
       "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
   };
+
+  ### nixbuild.net
+  /*
+  programs.ssh = {
+    extraConfig = ''
+      Host eu.nixbuild.net
+      PubkeyAcceptedKeyTypes ssh-ed25519
+      ServerAliveInterval 60
+      IPQoS throughput
+      IdentityFile /root/.ssh/id_ed25519.nixbuild
+    '';
+
+    knownHosts = {
+      nixbuild = {
+        hostNames = [ "eu.nixbuild.net" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+      };
+    };
+  };
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "x86_64-linux";
+        maxJobs = 100;
+        supportedFeatures = [ "benchmark" "big-parallel" ];
+      }
+    ];
+  };
+  */
+  ### nixbuild.net
 }
