1a3667a5576e66256ca3ea809ae09952d1851f5b
Whovian9369 <Whovian9369@gmail.com>
2025-07-03 01:15:13 -0400

    Add another `udev` rule, this time for iOS checkra1n stuff.

--- system/chimchar/udev.nix
+++ system/chimchar/udev.nix
@@ -9,12 +9,19 @@
 
       # Nintendo Switch - nxdumptool USB Dumping
       SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", MODE="0666"
+
+      # Handle PongoOS
+      # ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="4141", OWNER="root", GROUP="plugdev", MODE="0660"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="4141", MODE="0666"
     '';
       /*
         First rule is access to do Nintendo Switch RCM Payload injection.
 
         Second rule is access to nxdumptool/nxdumpclient USB Dumping.
         Taken from github:v1993/nxdumpclient - /data/71-nxdumptool.rules
+
+        Third rule is to access PongoOS, a lower level shell at iBoot(?) level
+        Taken from github:matteyeux/autodecrypt - /66-pongos.rules
       */
   };
 }
