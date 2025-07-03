897bf0c91a597b2f168ba2e1549f188c6d8fecb3
Whovian9369 <Whovian9369@gmail.com>
2025-07-03 01:19:25 -0400

    Tweak networking stuff.

--- system/chimchar/networking.nix
+++ system/chimchar/networking.nix
@@ -1,14 +1,29 @@
 { pkgs, ... }:
 {
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs.mullvad-vpn;
+  services = {
+    mullvad-vpn = {
+      enable = true;
+      package = pkgs.mullvad-vpn;
+    };
+    resolved = {
+      enable = false;
+      dnssec = "true";
+      fallbackDns = [
+         "1.1.1.1"
+         "1.0.0.1"
+       ];
+  dnsovertls = "true";
+    };
   };
-  networking.firewall = {
-    interfaces = {
-      enp1s0f0 = {
-        allowedTCPPorts = [ 5900 5800 ];
-        allowedUDPPorts = [ 5900 5800 ];
+
+  networking = {
+    resolvconf.enable = true;
+    firewall = {
+      interfaces = {
+        enp1s0f0 = {
+          allowedTCPPorts = [ 5900 5800 ];
+          allowedUDPPorts = [ 5900 5800 ];
+        };
       };
     };
   };
